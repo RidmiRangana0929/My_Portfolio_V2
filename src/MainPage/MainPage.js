e95556d2db782a69c8b9c4527dc546bdedf7f9b5
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-15 16:17:07 +0530

    added basic structure and styles

---
+++ src/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Style.css'
+
+export const MainPage = () => {
+  return (
+    <div>
+
+        <div className='navBar'></div>
+        <div className='homeImg'>
+            <div className='home'></div>
+        </div>
+        <div className='about'></div>
+
+    </div>
+  )
+}
