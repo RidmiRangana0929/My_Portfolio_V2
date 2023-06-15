b0866b7561b753b7533bba0b938de0188b1fe5af
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-15 20:30:14 +0530

    updated

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -5,7 +5,24 @@ export const MainPage = () => {
   return (
     <div>
 
-        <div className='navBar'></div>
+        <div className='navBar'>
+            <div className='btnGrid'>
+            <button className='navBtn'>Home</button>
+            <button className='navBtn'>About</button>
+            <button className='navBtn'>Resume</button>
+            <button className='navBtn'>Projects</button>
+            <button className='navBtn'>Services</button>
+            <button className='navBtn'>Contacts</button>
+            </div>
+            <div className='socialMediaIconGrid'>
+                
+                <div className='socialMedia' id='facebook'></div>
+                <div className='socialMedia' id='instagrame'></div>
+                <div className='socialMedia' id='whatsapp'></div>
+                <div className='socialMedia' id='slack'></div>
+                <div className='socialMedia' id='github'></div>
+            </div>
+        </div>
         <div className='homeImg'>
             <div className='home'></div>
         </div>
