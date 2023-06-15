4a4b0f179337c2e5fbf9d5a395b9197b71829b7b
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-15 21:20:55 +0530

    updated

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -7,15 +7,14 @@ export const MainPage = () => {
 
         <div className='navBar'>
             <div className='btnGrid'>
-            <button className='navBtn'>Home</button>
-            <button className='navBtn'>About</button>
-            <button className='navBtn'>Resume</button>
-            <button className='navBtn'>Projects</button>
-            <button className='navBtn'>Services</button>
-            <button className='navBtn'>Contacts</button>
+                <button className='navBtn'>Home</button>
+                <button className='navBtn'>About</button>
+                <button className='navBtn'>Resume</button>
+                <button className='navBtn'>Projects</button>
+                <button className='navBtn'>Services</button>
+                <button className='navBtn'>Contacts</button>
             </div>
             <div className='socialMediaIconGrid'>
-                
                 <div className='socialMedia' id='facebook'></div>
                 <div className='socialMedia' id='instagrame'></div>
                 <div className='socialMedia' id='whatsapp'></div>
@@ -26,7 +25,12 @@ export const MainPage = () => {
         <div className='homeImg'>
             <div className='home'></div>
         </div>
-        <div className='about'></div>
+        <div className='about'>
+            <h1 className='title' id='aboutTitle'>ABOUT ME</h1>
+        </div>
+        <div className='resume'>
+            <h1 className='title' id='resumeTitle'>SAMMARY</h1>
+        </div>
 
     </div>
   )
