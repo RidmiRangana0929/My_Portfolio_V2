438d2483e895fe3c7aa98f311a5024f6a04fb92a
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-16 12:39:58 +0530

    updated

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -41,6 +41,48 @@ export const MainPage = () => {
                 <h2 className='subTitle'>Know Me More</h2>
                 <div className='underline'></div>
             </div>
+
+            <div id='aboutPageLayout'>
+                <div id='greeting'>
+                    <h3 id='greetingPart1'>I'm</h3> <h3 id='greetingPart2'>Ridmi Silva</h3> <h3 id='greetingPart3'>,</h3>
+                </div>
+                <div id='distription'>
+                    <div id='paragraph'></div>
+                    <div id='personalDetails'>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+                <div id='caculation'>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </div>
 
 
@@ -50,6 +92,7 @@ export const MainPage = () => {
             <div className='titleContainer'>
                 <h1 className='title' id='resumeTitle'>SAMMARY</h1>
                 <h2 className='subTitle'>Resume</h2>
+                <div className='underline'></div>
             </div>
         </div>
 
