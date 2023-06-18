4a284ba304d8ec65a39a73c4417bcc1e18d72033
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-18 16:51:20 +0530

    updated home page and set typewriter effect to text

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Typewriter from 'typewriter-effect';
 import './Style.css'
 
 
@@ -32,7 +32,28 @@ export const MainPage = () => {
         {/* home page... */}
 
         <div className='homeImg'>
-            <div className='home'></div>
+            <div className='home'>
+                <div id='textBox'>
+                <h3 id='welcome'>Welcome</h3>
+                <div className='gap'></div>
+                <div id='typewriter'>
+                    I'm a
+                    <Typewriter
+                        options={{
+                            strings: [
+                              "Software Developer.",
+                              "Web Developer.",
+                              "UI/UX Designer."
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                        />    
+                </div>
+                <div className='gap'></div>
+                <button id='hireMe' className='button'>Hire Me</button>
+                </div>
+            </div>
         </div>
 
 
